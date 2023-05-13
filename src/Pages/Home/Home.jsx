1c8baf5e44a8cb9f7de70062fce33d398218d801
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-14 00:36:20 +0600

    add populer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopBanner from '../../Components/Home/TopBanner';
+import PremiumService from '../../Components/Home/PremiumService';
 
 const Home = () => {
     return (
         <div className=''>
-            <TopBanner/>
+            <TopBanner />
+            <PremiumService />
         </div>
     );
 };
