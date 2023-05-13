2ea5f0d90d2b31b643d79788b148b90c5ca6cbcc
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-13 21:00:39 +0600

    initial setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className="text-2xl text-center text-rose-600">Welcome to CarPort</h3>
+        </div>
+    );
+};
+
+export default Home;
