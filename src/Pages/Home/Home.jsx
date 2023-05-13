92a43bd5548b75e275742ec4c5e6f13bc524181e
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-14 00:52:47 +0600

    add valuable speech

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TopBanner from '../../Components/Home/TopBanner';
 import PremiumService from '../../Components/Home/PremiumService';
+import ValuableSpeech from '../../Components/Home/ValuableSpeech';
 
 const Home = () => {
     return (
         <div className=''>
             <TopBanner />
             <PremiumService />
+            <ValuableSpeech/>
         </div>
     );
 };
