a19d320183fb7756c941b87cceb9ca24f25df879
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-14 01:14:28 +0600

    add overview

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import TopBanner from '../../Components/Home/TopBanner';
 import PremiumService from '../../Components/Home/PremiumService';
 import ValuableSpeech from '../../Components/Home/ValuableSpeech';
+import Overview from '../../Components/Home/Overview';
 
 const Home = () => {
     return (
         <div className=''>
             <TopBanner />
             <PremiumService />
-            <ValuableSpeech/>
+            <ValuableSpeech />
+            <Overview />
         </div>
     );
 };
