e464d94b83d4647bd363ce8acb43a7587730d93e
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-14 11:59:50 +0600

    add projects

--- src/Components/Home/ExclusivePackages.jsx
+++ src/Components/Home/ExclusivePackages.jsx
@@ -13,7 +13,7 @@ const ExclusivePackages = () => {
             </div>
 
             <section className="w-full pt-10">
-                <div className="flex flex-col md:flex-row justify-between items-center w-full gap-5">
+                <div className="flex flex-col md:flex-row justify-between py-7 items-center w-full gap-5">
                     <div className="w-full md:w-3/6 mx-auto">
                         <img src="https://i.ibb.co/XpsNBjd/Rectangle-14.png" alt="" className="" />
                     </div>
@@ -47,6 +47,114 @@ const ExclusivePackages = () => {
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className="flex flex-col md:flex-row-reverse justify-between py-10 items-center w-full gap-5">
+                    <div className="w-full md:w-3/6 mx-auto">
+                        <img src="https://i.ibb.co/k3Wc87Y/Rectangle-114.png" alt="" className="" />
+                    </div>
+                    <div className="w-full md:w-3/6 mx-auto p-5">
+                        <h5 className="text-md text-gray-400">Project 02</h5>
+                        <h3 className="text-xl font-semibold text-gray-700 pt-3">Free School Street Carport</h3>
+                        <div className="pt-5 space-y-3">
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">03 Car Parking And Shelter space</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully metal body roof</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">protect your car from sun and rain</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col md:flex-row justify-between py-10 items-center w-full gap-5">
+                    <div className="w-full md:w-3/6 mx-auto">
+                        <img src="https://i.ibb.co/r5Yssd7/Rectangle-114-1.png" alt="" className="" />
+                    </div>
+                    <div className="w-full md:w-3/6 mx-auto p-5">
+                        <h5 className="text-md text-gray-400">Project 03</h5>
+                        <h3 className="text-xl font-semibold text-gray-700 pt-3">Free School Street Carport</h3>
+                        <div className="pt-5 space-y-3">
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">03 Car Parking And Shelter space</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully metal body roof</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">protect your car from sun and rain</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col md:flex-row-reverse justify-between py-10 items-center w-full gap-5">
+                    <div className="w-full md:w-3/6 mx-auto">
+                        <img src="https://i.ibb.co/86bfH9k/Rectangle-114-2.png" alt="" className="" />
+                    </div>
+                    <div className="w-full md:w-3/6 mx-auto p-5">
+                        <h5 className="text-md text-gray-400">Project 04</h5>
+                        <h3 className="text-xl font-semibold text-gray-700 pt-3">Free School Street Carport</h3>
+                        <div className="pt-5 space-y-3">
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">03 Car Parking And Shelter space</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully metal body roof</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">protect your car from sun and rain</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FaCheck className='text-[#00C2FF]' />
+                                <span className="">Fully Customized and Foldable</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-5">
+                    <button className='bg-[#00C2FF] btn hover:bg-warning text-gray-700 px-5 py-2 rounded'>View More Work</button>
                 </div>
             </section>
 
