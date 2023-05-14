b3c0e5788ec23dc7cf31a3039cf32eaf0c37c792
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-14 19:45:32 +0600

    header resolved

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -5,13 +5,15 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='w-full container sticky top-0 z-50'>
-            <div className="navbar bg-base-100 z-50 w-full md:w-4/5 mx-auto sticky top-0">
-                <div className="navbar-start">
-                    <a className="btn btn-ghost normal-case text-xl">
-                        <img src="https://i.ibb.co/qNmrrC8/Valetcarport.png" alt="" className="" />
-                    </a>
-                </div>
-                {/* <div className="navbar-center hidden lg:flex">
+            <div className="bg-base-100 z-50 sticky top-0">
+                <div className="navbar w-full md:w-4/5 mx-auto">
+
+                    <div className="navbar-start">
+                        <a className="btn btn-ghost normal-case text-xl">
+                            <img src="https://i.ibb.co/qNmrrC8/Valetcarport.png" alt="" className="" />
+                        </a>
+                    </div>
+                    {/* <div className="navbar-center hidden lg:flex">
                     <ul className="menu gap-2 menu-horizontal px-1">
                         <li><NavLink to="/">Home</NavLink></li>
                         <li tabIndex={0}>
@@ -30,29 +32,30 @@ const Header = () => {
                         <li><NavLink to="/features">Features</NavLink></li>
                     </ul>
                 </div> */}
-                <div className="navbar-end">
-                    <div className="mx-3">
-                        <FaSearch />
-                    </div>
-                    <div className="mx-3">
-                        <FaCartPlus />
-                    </div>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 gap-2 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><NavLink to="/">Carport</NavLink></li>
-                            <li><NavLink to="/sofa">Sofa</NavLink></li>
-                            <li><NavLink to="/massagechair">Massage Chair</NavLink></li>
-                            <li><NavLink to="/lights">Lights</NavLink></li>
+                    <div className="navbar-end">
+                        <div className="mx-3">
+                            <FaSearch />
+                        </div>
+                        <div className="mx-3">
+                            <FaCartPlus />
+                        </div>
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 gap-2 p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><NavLink to="/">Carport</NavLink></li>
+                                <li><NavLink to="/sofa">Sofa</NavLink></li>
+                                <li><NavLink to="/massagechair">Massage Chair</NavLink></li>
+                                <li><NavLink to="/lights">Lights</NavLink></li>
 
-                            {/* <div className="form-control">
+                                {/* <div className="form-control">
                                 <input type="text" placeholder="Search" className="input input-sm w-40 mx-auto input-bordered" />
                             </div> */}
 
-                        </ul>
+                            </ul>
 
+                        </div>
                     </div>
                 </div>
             </div>
