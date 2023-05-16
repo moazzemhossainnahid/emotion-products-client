11354399e2db9e7df7e8c783f22fdb7429c08f07
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-16 23:45:25 +0600

    add gallery images

--- src/Components/SinglePages/Tabs/Gallery/Gallery.jsx
+++ src/Components/SinglePages/Tabs/Gallery/Gallery.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { SlideshowLightbox } from 'lightbox.js-react';
+import 'lightbox.js-react/dist/index.css';
 
 const Gallery = () => {
     return (
-        <div>
-            
+        <div className='w-full md:w-4/5 mx-auto py-10'>
+            <SlideshowLightbox className="container grid grid-cols-3 gap-2 mx-auto">
+                <img className="w-full rounded" src="https://source.unsplash.com/pAKCx4y2H6Q/1400x1200" />
+                <img className="w-full rounded" src="https://source.unsplash.com/AYS2sSAMyhc/1400x1200" />
+                <img className="w-full rounded" src="https://source.unsplash.com/Kk8mEQAoIpI/1600x1200" />
+                <img className="w-full rounded" src="https://source.unsplash.com/Kk8mEQAoIpI/1600x1200" />
+                <img className="w-full rounded" src="https://source.unsplash.com/AYS2sSAMyhc/1400x1200" />
+                <img className="w-full rounded" src="https://source.unsplash.com/pAKCx4y2H6Q/1400x1200" />
+            </SlideshowLightbox>
         </div>
     );
 };
