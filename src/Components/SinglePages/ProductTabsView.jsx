78445e02e327ea7108efa09c4f1ac8ce24dba426
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-16 23:56:09 +0600

    button color add

--- src/Components/SinglePages/ProductTabsView.jsx
+++ src/Components/SinglePages/ProductTabsView.jsx
@@ -15,9 +15,9 @@ const ProductTabsView = () => {
             <div className="w-full">
                 <div className="space-y-5">
                     <div className="flex gap-3 items-center justify-center w-full mx-auto mt-7">
-                        <button onClick={() => setTab("info")} className="font-medium border border-gray-400 cursor-pointer hover:text-primary hover:border-primary rounded-3xl py-2 px-7">Overview</button>
-                        <button onClick={() => setTab("answer")} className="font-medium border ml-2 rounded-3xl cursor-pointer border-gray-400 hover:text-primary hover:border-primary py-2 px-7">Gallery</button>
-                        <button onClick={() => setTab("review")} className="font-medium border ml-2 rounded-3xl cursor-pointer hover:text-primary hover:border-primary border-gray-400 py-2 px-7">Reviews</button>
+                        <button onClick={() => setTab("info")} className="font-medium border focus:bg-primary border-gray-400 cursor-pointer hover:text-primary hover:border-primary rounded-3xl py-2 px-7">Overview</button>
+                        <button onClick={() => setTab("answer")} className="font-medium border focus:bg-primary ml-2 rounded-3xl cursor-pointer border-gray-400 hover:text-primary hover:border-primary py-2 px-7">Gallery</button>
+                        <button onClick={() => setTab("review")} className="font-medium border focus:bg-primary ml-2 rounded-3xl cursor-pointer hover:text-primary hover:border-primary border-gray-400 py-2 px-7">Reviews</button>
                     </div>
                     {tab === "info" && <Overview />}
                     {tab === "answer" && <Gallery />}
