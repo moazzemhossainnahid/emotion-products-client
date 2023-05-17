3593d3a4a067fce04b103b4e483e38810d6f8fde
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-17 12:53:11 +0600

    add carousel

--- src/Components/Others/SuccessStoriesCarousel.jsx
+++ src/Components/Others/SuccessStoriesCarousel.jsx
@@ -1,55 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import useEmblaCarousel from 'embla-carousel-react';
-import Autoplay from 'embla-carousel-autoplay';
-import { flushSync } from 'react-dom';
-
-import "./SwiperStyle.css";
+import React from 'react';
 import { FaQuoteLeft } from 'react-icons/fa';
-const TWEEN_FACTOR = 4.2
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import "./SStoriesStyle.css";
 
-const numberWithinRange = (number, min, max) =>
-    Math.min(Math.max(number, min), max)
 
 const SuccessStoriesCarousel = () => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
-
-    const [tweenValues, setTweenValues] = useState([]);
-
-    const onScroll = useCallback(() => {
-        if (!emblaApi) return
-
-        const engine = emblaApi.internalEngine()
-        const scrollProgress = emblaApi.scrollProgress()
-
-        const styles = emblaApi.scrollSnapList().map((scrollSnap, index) => {
-            let diffToTarget = scrollSnap - scrollProgress
-
-            if (engine.options.loop) {
-                engine.slideLooper.loopPoints.forEach((loopItem) => {
-                    const target = loopItem.target()
-                    if (index === loopItem.index && target !== 0) {
-                        const sign = Math.sign(target)
-                        if (sign === -1) diffToTarget = scrollSnap - (1 + scrollProgress)
-                        if (sign === 1) diffToTarget = scrollSnap + (1 - scrollProgress)
-                    }
-                })
-            }
-            const tweenValue = 1 - Math.abs(diffToTarget * TWEEN_FACTOR)
-            return numberWithinRange(tweenValue, 0, 1)
-        })
-        setTweenValues(styles)
-    }, [emblaApi, setTweenValues])
-
-    useEffect(() => {
-        if (!emblaApi) return
-
-        onScroll()
-        emblaApi.on('scroll', () => {
-            flushSync(() => onScroll())
-        })
-        emblaApi.on('reInit', onScroll)
-    }, [emblaApi, onScroll])
-
 
     const ourFeedback = [
         {
@@ -57,114 +13,115 @@ const SuccessStoriesCarousel = () => {
             name: "Jenifer Lopez",
             feedbackStarts: 3,
             feedbackComment: "Hey guys! I just wanted to drop a quick review for this amazing carport company. I recently had a carport installed on my property and I couldn't be happier with the results. The team was friendly and professional, and they really took the time to listen to my needs and wants.",
-            img: "https://i.ibb.co/Lpgw2R8/Rectangle-6.png"
+            img: "https://i.ibb.co/mFHqmfj/Ellipse-28.png"
         },
         {
             id: 2,
             name: "Jenifer Lopez",
             feedbackStarts: 2,
             feedbackComment: "Hey guys! I just wanted to drop a quick review for this amazing carport company. I recently had a carport installed on my property and I couldn't be happier with the results. The team was friendly and professional, and they really took the time to listen to my needs and wants.",
-            img: "https://i.ibb.co/Lpgw2R8/Rectangle-6.png"
+            img: "https://i.ibb.co/mFHqmfj/Ellipse-28.png"
         },
         {
             id: 3,
             name: "Jenifer Lopez",
             feedbackStarts: 1,
             feedbackComment: "Hey guys! I just wanted to drop a quick review for this amazing carport company. I recently had a carport installed on my property and I couldn't be happier with the results. The team was friendly and professional, and they really took the time to listen to my needs and wants.",
-            img: "https://i.ibb.co/Lpgw2R8/Rectangle-6.png"
+            img: "https://i.ibb.co/mFHqmfj/Ellipse-28.png"
         },
         {
             id: 4,
             name: "Jenifer Lopez",
             feedbackStarts: 5,
             feedbackComment: "Hey guys! I just wanted to drop a quick review for this amazing carport company. I recently had a carport installed on my property and I couldn't be happier with the results. The team was friendly and professional, and they really took the time to listen to my needs and wants.",
-            img: "https://i.ibb.co/Lpgw2R8/Rectangle-6.png"
+            img: "https://i.ibb.co/mFHqmfj/Ellipse-28.png"
         },
     ];
 
 
     return (
         <div className=" banner">
-            <div className="embla" ref={emblaRef}>
-                <div className="embla__container">
-                    {ourFeedback?.map((feedback, index) => (
-                        <div
-                            className="embla__slide"
-                            key={feedback?.id}
-                            style={{
-                                ...(tweenValues.length && { opacity: tweenValues[index] }),
-                            }}
-                        >
-                            <div className="text-center space-y-2 embla__slide__img">
-                                <h2 className="text-2xl.font-bold">{feedback?.name}</h2>
-                                <img src={feedback?.img} alt="" className="w-20 h-20 mx-auto rounded-full" />
-                                {
+            <section id="slider" class="pt-5">
+                <div class="container">
+                    <div class="slider">
+                        <div class="owl-carousel">
+                            {ourFeedback?.map((feedback, index) => (
+                                <div
+                                    className="slider-card bg-transparent"
+                                    key={feedback?.id}
+                                >
+                                    <div className="text-center space-y-2 bg-transparent ">
+                                        <h2 className="text-2xl.font-bold">{feedback?.name}</h2>
+                                        <img src={feedback?.img} alt="" className="w-20 h-20 mx-auto rounded-full" />
+                                        {
 
-                                    feedback?.feedbackStarts === 1 &&
-                                    <div className="rating">
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" checked />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
+                                            feedback?.feedbackStarts === 1 &&
+                                            <div className="rating">
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" checked />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                            </div>
+
+                                        }
+                                        {
+
+                                            feedback?.feedbackStarts === 2 &&
+                                            <div className="rating">
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" checked />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                            </div>
+
+                                        }
+                                        {
+
+                                            feedback?.feedbackStarts === 3 &&
+                                            <div className="rating">
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" checked />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                            </div>
+
+                                        }
+                                        {
+
+                                            feedback?.feedbackStarts === 4 &&
+                                            <div className="rating">
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" checked />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                            </div>
+
+                                        }
+                                        {
+
+                                            feedback?.feedbackStarts === 5 &&
+                                            <div className="rating">
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" />
+                                                <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-orange-500" checked />
+                                            </div>
+
+                                        }
+                                        <p className="text-xs break-words md:text-sm px-7">{feedback?.feedbackComment.length > 100 ? `${feedback?.feedbackComment.slice(0, 100)}...` : feedback?.feedbackComment}</p>
+                                        <h3 className="text-gray-400 text-3xl text-center flex justify-center pt-5"><FaQuoteLeft /></h3>
                                     </div>
-
-                                }
-                                {
-
-                                    feedback?.feedbackStarts === 2 &&
-                                    <div className="rating">
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" checked />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                    </div>
-
-                                }
-                                {
-
-                                    feedback?.feedbackStarts === 3 &&
-                                    <div className="rating">
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" checked />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                    </div>
-
-                                }
-                                {
-
-                                    feedback?.feedbackStarts === 4 &&
-                                    <div className="rating">
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" checked />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-gray-300" />
-                                    </div>
-
-                                }
-                                {
-
-                                    feedback?.feedbackStarts === 5 &&
-                                    <div className="rating">
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" />
-                                        <input name="rating-1" className="mask mask-star w-4 md:w-6  bg-green-500" checked />
-                                    </div>
-
-                                }
-                                <p className="text-xs break-words md:text-sm">{feedback?.feedbackComment.length > 100 ? `${feedback?.feedbackComment.slice(0, 100)}...` : feedback?.feedbackComment}</p>
-                                <h3 className="text-gray-400 text-3xl text-center flex justify-center pt-5"><FaQuoteLeft /></h3>
-                            </div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
     );
 };
