9e7d16558d40b3710a8d3257c259967aa8c944b8
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-06 11:44:56 +0600

    add ct modal

--- src/Components/Home/HomeTopBannerCarousel.jsx
+++ src/Components/Home/HomeTopBannerCarousel.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { FaArrowRight } from 'react-icons/fa';
+import { FaArrowRight, FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
 import Slide from 'react-reveal/Slide';
 import Slider from 'react-slick';
+import { useForm } from "react-hook-form";
 
 const HomeTopBannerCarousel = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
     const settings = {
         dots: true,
         dotsClass: "slick-dots slick-thumb",
@@ -44,6 +47,10 @@ const HomeTopBannerCarousel = () => {
             </div>
 
         )
+    };
+
+    const onSubmit = (data) => {
+        console.log(data);
     };
 
     const SliderData = [
@@ -90,7 +97,9 @@ const HomeTopBannerCarousel = () => {
                                     </Slide>
                                     <Slide bottom duration={2500}>
                                         <button className='bg-[#00C2FF] border-0 justify-center btn btn-warning px-7 py-2 text-black rounded'>
-                                            <p className=" flex gap-2">Make An Appointment <FaArrowRight /></p>
+                                            <label htmlFor="my-modal-3" className="mx-auto text-white">
+                                                <p className=" flex gap-2">Make An Appointment <FaArrowRight /></p>
+                                            </label>
                                         </button>
                                     </Slide>
                                 </div>
@@ -99,7 +108,173 @@ const HomeTopBannerCarousel = () => {
                     })
                 }
             </Slider>
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal z-50 h-screen w-full overflow-hidden">
+                <div className="modal-box w-11/12 max-w-5xl bg-[url('https://i.ibb.co/37G57Y2/Rectangle-111-1.png')] bg-cover overflow-hidden relative bg-[#000000] ">
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <div className="text-white">
+                        <div className="flex items-center min-h-full p-4 lg:justify-center">
+                            <div className="flex flex-col overflow-hidden mx-auto bg-transparent rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-screen-md drop-shadow-2xl">
+                                <div className="hidden p-3 text-white text-start bg-transparent md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly">
+                                    <h3 className="text-5xl font-bold text-start">Contact US</h3>
+                                    <h3 className="text-2xl font-semibold">We'd Love to Hear from You</h3>
+                                    <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quo commodi ratione obcaecati dolores quos sunt sed rerum laborum? Vel!</p>
+                                    <div className="flex gap-2">
+                                        <span className="text-xl p-1 bg-white text-gray-600"><FaInstagram /></span>
+                                        <span className="text-xl p-1 bg-white text-gray-600"><FaFacebook /></span>
+                                        <span className="text-xl p-1 bg-white text-gray-600"><FaYoutube /></span>
+                                        <span className="text-xl p-1 bg-white text-gray-600"><FaTwitter /></span>
+                                        <span className="text-xl p-1 bg-white text-gray-600"><FaLinkedin /></span>
+                                    </div>
+                                </div>
+                                <div className="p-3 bg-transparent md:flex-1">
+                                    <h3 className="my-4 text-3xl font-bold text-yellow-300 text-center ">
+                                        Contact Form
+                                    </h3>
+                                    <form
+                                        onSubmit={handleSubmit(onSubmit)}
+                                        action="#"
+                                        className="flex flex-col space-y-2"
+                                    >
+                                        <div className="flex flex-col space-y-1">
+                                            <label
+                                                htmlFor="name"
+                                                className="text-sm font-semibold text-white text-start"
+                                            >
+                                                Name
+                                            </label>
+                                            <input
+                                                type="text"
+                                                placeholder="Name"
+                                                className="px-4 py-2 transition duration-300 border border-gray-300 text-black rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                                {...register("name", {
+                                                    required: {
+                                                        value: true,
+                                                        message: "Name is require",
+                                                    }
+                                                })}
+                                            />
+                                            {/* <label className="label">
+                                                {errors.name?.type === "required" && (
+                                                    <span className="label-text-alt text-red-700">
+                                                        {errors.name.message}
+                                                    </span>
+                                                )}
+                                                {errors.name?.type === "pattern" && (
+                                                    <span className="label-text-alt text-red-700">
+                                                        {errors.name.message}
+                                                    </span>
+                                                )}
+                                            </label> */}
+                                        </div>
+                                        <div className="flex flex-col space-y-1">
+                                            <label
+                                                htmlFor="phone"
+                                                className="text-sm font-semibold text-white text-start"
+                                            >
+                                                Phone Number
+                                            </label>
+                                            <input
+                                                type="phone"
+                                                placeholder="Phone Number"
+                                                className="px-4 py-2 transition duration-300 border border-gray-300 text-black rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                                {...register("phone", {
+                                                    required: {
+                                                        value: true,
+                                                        message: "Phone Number is require",
+                                                    }
+                                                })}
+                                            />
+                                            {/* <label className="label">
+                                                {errors.phone?.type === "required" && (
+                                                    <span className="label-text-alt text-red-700">
+                                                        {errors.phone.message}
+                                                    </span>
+                                                )}
+                                                {errors.phone?.type === "pattern" && (
+                                                    <span className="label-text-alt text-red-700">
+                                                        {errors.phone.message}
+                                                    </span>
+                                                )}
+                                            </label> */}
+                                        </div>
+                                        <div className="flex flex-col space-y-1">
+                                            <label
+                                                htmlFor="email"
+                                                className="text-sm font-semibold text-white text-start"
+                                            >
+                                                Email
+                                            </label>
+                                            <input
+                                                type="email"
+                                                placeholder="Email"
+                                                className="px-4 py-2 transition duration-300 border border-gray-300 text-black rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                                {...register("email", {
+                                                    required: {
+                                                        value: true,
+                                                        message: "Email is require",
+                                                    },
+                                                    pattern: {
+                                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                                        message: "Provide a valid Email",
+                                                    },
+                                                })}
+                                            />
+                                            {/* <label className="label">
+                                                {errors.email?.type === "required" && (
+                                                    <span className="label-text-alt text-red-700">
+                                                        {errors.email.message}
+                                                    </span>
+                                                )}
+                                                {errors.email?.type === "pattern" && (
+                                                    <span className="label-text-alt text-red-700">
+                                                        {errors.email.message}
+                                                    </span>
+                                                )}
+                                            </label> */}
+                                        </div>
+                                        <div className="flex flex-col space-y-1">
+                                            <label
+                                                htmlFor="messege"
+                                                className="text-sm font-semibold text-white text-start"
+                                            >
+                                                Messege
+                                            </label>
+                                            <textarea
+                                                type="text"
+                                                placeholder="Messege"
+                                                className="px-4 py-2 transition duration-300 border border-gray-300  text-black rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                                {...register("messege", {
+                                                    required: {
+                                                        value: true,
+                                                        message: "Messege is require",
+                                                    },
+                                                    minLength: {
+                                                        value: 6,
+                                                        message: "Must be 6 characters or longer",
+                                                    },
+                                                })}
+                                            />
 
+                                        </div>
+
+                                        <div className="pt-3">
+
+                                            <button
+                                                type="submit"
+                                                className="w-full px-4 py-2 text-lg font-semibold text-gray-700 transition-colors duration-300 bg-gradient-to-r from-[#bfcc35] to-[#fff30a] rounded-md shadow  hover:bg-gradient-to-l focus:outline-none focus:ring-blue-200 focus:ring-4"
+                                            >
+                                                Send
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
