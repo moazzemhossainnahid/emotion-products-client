0fb1c8118327ba2b62c8728affdae482b5c1b0fc
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-06 15:44:05 +0600

    add design

--- src/Components/CarPort/ProtectVehicle.jsx
+++ src/Components/CarPort/ProtectVehicle.jsx
@@ -7,7 +7,7 @@ const ProtectVehicle = () => {
                 <div className='w-full md:w-3/6 p-0 sm:p-5'>
                     <h2 className='text-center text-2xl sm:text-5xl sm:text-left'>Protect Your Vehicle</h2>
                     <p className='text-center text-sm sm:text-left pt-3'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <button className="bg-primary mt-5 px-7 py-2 rounded">Buy Now</button>
+                    <button className="bg-primary flex justify-center w-1/3 mx-auto md:mx-0 mt-5 px-7 py-2 rounded">Buy Now</button>
                 </div>
                 <div className='w-full md:w-3/6 mx-auto md:-mr-10'>
                     <img className='w-full object-cover' src="https://i.ibb.co/yPJ4BK8/Rectangle-184.png" alt="" />
