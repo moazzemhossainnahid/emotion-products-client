12c0ffc6df2320bfc5f8c426c7792535fc8d4ee7
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-17 08:20:56 +0600

    dex p

--- src/Components/Home/DiscoverExclusiveProducts.jsx
+++ src/Components/Home/DiscoverExclusiveProducts.jsx
@@ -4,10 +4,10 @@ import { FaArrowRight } from 'react-icons/fa';
 const DiscoverExclusiveProducts = () => {
     return (
         <div className='container w-full py-7'>
-            <div className="w-full mx-auto bg-[url('https://i.ibb.co/Yb1FC84/Rectangle-174.png')] relative h-[80vh] bg-cover">
-                <div className=" bg-white absolute bottom-7 p-10 left-7">
-                    <h3 className="text-gray-600 text-2xl md:text-3xl">Discover Our <br /> Exclusive Prducts</h3>
-                    <button className='flex gap-2 items-center pt-5'>Discover More <FaArrowRight className='text-md'/></button>
+            <div className="w-full mx-auto bg-[url('https://i.ibb.co/Yb1FC84/Rectangle-174.png')] p-10 relative bg-cover">
+                <div className=" bg-[#efeeeec6] p-7 rounded w-full md:w-4/5 mx-auto">
+                <h3 className="text-gray-600 text-2xl pb-7 md:text-3xl">Discover Our Exclusive Prducts</h3>
+                    <p className='text-sm'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
                 </div>
             </div>
         </div>
