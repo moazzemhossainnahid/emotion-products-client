09bc28d6a42cad57067986d7472a866331372c3a
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-17 08:13:33 +0600

    complete collection

--- src/Components/Home/CompleteCollection.jsx
+++ src/Components/Home/CompleteCollection.jsx
@@ -8,24 +8,16 @@ const CompleteCollection = () => {
     return (
         <div className='w-full py-20 text-center p-5'>
             <h3 className="text-2xl md:text-3xl">Our Complete Collection With Luxury</h3>
-            <p className="text-sm">It is a long established fact that a reader will be distracted by the readable content of a page</p>
+            <p className="text-sm pt-2">It is a long established fact that a reader will be distracted by the readable content of a page</p>
 
-            <div className="w-full md:w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center gap-3 p-3 py-10">
+            <div className="w-full md:w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center gap-10 p-3 py-10">
                 {
                     CompleteCollectionData?.map(d => (
-                        <div key={d?.id} className="w-full mx-auto flex flex-col justify-center">
-                            <div className="relative w-full mx-auto">
-                                <img src={d?.img} alt="" className="relative w-full" />
-                                <h3 onClick={() => navigate(d?.href)} className="absolute bottom-5 left-5 bg-white px-7 flex gap-2 cursor-pointer items-center py-2 text-black">Explore Now <FaArrowRight/></h3>
+                        <div key={d?.id} className="w-full mx-auto flex md:flex-col justify-center items-center">
+                            <div className="relative w-full h-40 mx-auto">
+                                <img src={d?.img} alt="" className="relative h-full w-2/3 md:w-full hover:scale-95 duration-300 ease-in-out" />
                             </div>
-                            <h3 className="text-xl md:text-2xl font-semibold text-left">{d?.title}</h3>
-                            <div className="flex gap-2">
-                                <FaStar className='text-orange-600 text-md'/>
-                                <FaStar className='text-orange-600 text-md'/>
-                                <FaStar className='text-orange-600 text-md'/>
-                                <FaStar className='text-orange-600 text-md'/>
-                                <FaStar className='text-orange-600 text-md'/>
-                            </div>
+                            <h3 onClick={() => navigate(d?.href)} className="text-xl group cursor-pointer md:text-2xl capitalize font-extralight text-left pt-5 flex gap-2 items-center">{d?.title} <FaArrowRight className='text-xl group-hover:translate-x-3 duration-300 ease-in-out font-extralight '/> </h3>
                         </div>
                     ))
                 }
--- src/assets/Data/CompleteCollectionData.jsx
+++ src/assets/Data/CompleteCollectionData.jsx
@@ -3,28 +3,24 @@ export const CompleteCollectionData = [
         id: 1,
         title: "Carport",
         href: "carport",
-        img: "https://i.ibb.co/qdbwmF3/Rectangle-168.png",
-        ratings: 5
+        img: "https://i.ibb.co/10hSgpn/image-3.png",
     },
     {
         id: 2,
         title: "Massage Chair",
         href: "massagechair",
-        img: "https://i.ibb.co/c3Lrb7M/Rectangle-169.png",
-        ratings: 5
+        img: "https://i.ibb.co/bgbsj69/DSC03614-3.png",
     },
     {
         id: 3,
         title: "Furniture",
         href: "sofa",
-        img: "https://i.ibb.co/hs52mtY/Rectangle-170.png",
-        ratings: 5
+        img: "https://i.ibb.co/2Ybnw7x/sofa-1.png",
     },
     {
         id: 4,
         title: "Lights",
         href: "lights",
-        img: "https://i.ibb.co/ZVXFqsy/Rectangle-171.png",
-        ratings: 5
+        img: "https://i.ibb.co/7GbC1ZQ/image-4.png",
     },
 ]
