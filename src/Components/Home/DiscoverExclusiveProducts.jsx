c51b1672110c370949bf5efaef6e024953887452
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-18 19:29:30 +0600

    add working

--- src/Components/Home/DiscoverExclusiveProducts.jsx
+++ src/Components/Home/DiscoverExclusiveProducts.jsx
@@ -5,8 +5,8 @@ const DiscoverExclusiveProducts = () => {
     return (
         <div className='container w-full py-7'>
             <div className="w-full mx-auto bg-[url('https://i.ibb.co/Yb1FC84/Rectangle-174.png')] p-10 relative bg-cover">
-                <div className=" bg-[#efeeeec6] p-7 rounded w-full md:w-4/5 mx-auto">
-                <h3 className="text-gray-600 text-2xl pb-7 md:text-3xl">Discover Our Exclusive Prducts</h3>
+                <div className=" bg-gradient-to-b from-[#fcfcfc69] to-[#e5e4e4c8] p-7 rounded w-full md:w-4/5 mx-auto">
+                <h3 className="text-gray-700 text-2xl pb-7 md:text-3xl">Discover Our Exclusive Prducts</h3>
                     <p className='text-sm'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
                 </div>
             </div>
--- src/Components/Home/OurWorkingProcess.jsx
+++ src/Components/Home/OurWorkingProcess.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 const OurWorkingProcess = () => {
+
     return (
-        <div className='w-full'>
-            
+        <div className='w-full text-center py-10 p-5'>
+            <h3 style={{ fontFamily: 'Silk Serif' }} className="text-2xl md:text-4xl pb-5 text-gray-700 ">Our Complete Collection With Luxury</h3>
+            <p className="">It is a long established fact that a reader will be distracted by the readable content of a page</p>
+            <div className="py-7 w-full md:w-4/5 mx-auto text-left flex flex-col md:flex-row gap-5 justify-between items-center">
+                <div className="space-y-3">
+                    <div className="">
+                        <img src="https://i.ibb.co/xFKXQtf/Group-200.png" alt="icon" />
+                    </div>
+                    <h3 className="font-semibold text-xl">Consultation</h3>
+                    <p className="text-sm">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected</p>
+                </div>
+                <div className="space-y-3">
+                    <div className="">
+                        <img src="https://i.ibb.co/L0rgQ6z/Group-199.png" alt="icon" />
+                    </div>
+                    <h3 className="font-semibold text-xl">Choose Design</h3>
+                    <p className="text-sm">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected</p>
+                </div>
+                <div className="space-y-3">
+                    <div className="">
+                        <img src="https://i.ibb.co/9bq6z32/Group-201.png" alt="icon" />
+                    </div>
+                    <h3 className="font-semibold text-xl">Work done</h3>
+                    <p className="text-sm">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected</p>
+                </div>
+            </div>
+
         </div>
     );
 };
