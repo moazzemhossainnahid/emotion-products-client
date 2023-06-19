6921ee27627e42f27d34f3eff63fdd6be67a49d1
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-19 15:07:58 +0600

    resolve

--- src/Components/CarPort/TopBanner.jsx
+++ src/Components/CarPort/TopBanner.jsx
@@ -7,9 +7,9 @@ const TopBanner = () => {
             <div className="w-full text-center absolute bottom-40">
                 <h3 className="text-3xl md:text-5xl text-white">Protect Your Vehicle</h3>
                 <p className="pb-5 text-gray-300 text-sm">We make Sure that, to give you a premium Carport service</p>
-                <button className='bg-[#00C2FF] justify-center btn btn-warning px-7 py-2 text-black rounded'>
+                {/* <button className='bg-[#00C2FF] justify-center btn btn-warning px-7 py-2 text-black rounded'>
                     <p className=" flex gap-2">Make An Appointment <FaArrowRight /></p>
-                </button>
+                </button> */}
             </div>
         </div>
     );
--- src/Components/Lights/LightsTopBanner.jsx
+++ src/Components/Lights/LightsTopBanner.jsx
@@ -10,11 +10,11 @@ const LightsTopBanner = () => {
                     <h2 style={{ fontFamily: 'Silk Serif' }} className='text-center text-2xl sm:text-5xl sm:text-left'>Protect Your Vehicle</h2>
                     <h2 style={{ fontFamily: 'Silk Serif' }} className='text-center text-xl pt-5 sm:text-left'>Lorem ipsum dolor sit amet consectetur. </h2>
                     <p className='text-center text-sm sm:text-left pt-5 pb-7'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde officiis repellat doloremque facere consequuntur voluptas rem nesciunt ab distinctio alias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde officiis repellat doloremque facere consequuntur voluptas rem nesciunt ab distinctio alias.</p>
-                    <Slide bottom duration={2500}>
+                    {/* <Slide bottom duration={2500}>
                         <button className='bg-[#00C2FF] justify-center btn btn-warning px-7 py-2 text-black border-0 rounded'>
                             <p className=" flex gap-2">Request An Appointment <FaAngleRight /></p>
                         </button>
-                    </Slide>
+                    </Slide> */}
                 </div>
                 <div className='w-full md:w-3/6 relative overflow-hidden mx-auto'>
                     <img className='w-2/3 mx-auto z-20 relative' src="https://freepngimg.com/thumb/lamp/158530-ceiling-lamp-png-download-free.png" alt="" />
