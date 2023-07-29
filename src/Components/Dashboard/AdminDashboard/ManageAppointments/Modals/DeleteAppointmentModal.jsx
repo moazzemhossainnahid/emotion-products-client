f8dc63b7be2342c221db875c6dcdbb499215a4b1
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-07-29 13:37:31 +0600

    resolved

--- src/Components/Dashboard/AdminDashboard/ManageAppointments/Modals/DeleteAppointmentModal.jsx
+++ src/Components/Dashboard/AdminDashboard/ManageAppointments/Modals/DeleteAppointmentModal.jsx
@@ -23,13 +23,13 @@ const DeleteAppointmentModal = ({ deleteAppointment, setNumber, number }) => {
         <div>
             <input type="checkbox" id="delete-appointment-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
-                <div className="modal-box relative">
-                    <div className="absolute top-0 left-0 mix-blend-darken opacity-5">
+                <div className="modal-box relative ">
+                    <div className="absolute top-0 left-0 mix-blend-darken opacity-10">
                         <img src="https://www.security.org/app/uploads/2020/07/laptop-31.png" alt="" className="mix-blend-darken" />
                     </div>
                     <label htmlFor="delete-appointment-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h1 className='mb-4 badge badge-xs text-white badge-error text-sm p-4'>{`Delete ${name}'s Appointment`}</h1>
-                    <div className="w-full flex flex-col md:flex-row justify-between items-start gap-3">
+                    <h1 className='mb-4 badge badge-xs text-white badge-error relative text-sm p-4'>{`Delete ${name}'s Appointment`}</h1>
+                    <div className="w-full flex flex-col md:flex-row justify-between items-start gap-3 relative">
                         <div className="w-full md:w-4/5 order-2 md:order-1">
                             <h3 className="font-bold text-3xl">{product}</h3>
                             <h4 className="text-xl font-semibold pt-4">Customer Information-</h4>
@@ -43,7 +43,7 @@ const DeleteAppointmentModal = ({ deleteAppointment, setNumber, number }) => {
                             <img src={`https://media.istockphoto.com/id/538329926/vector/customer-support-vecot-icon-phone-assistant-logo.jpg?s=612x612&w=0&k=20&c=emj-_aWYs9yaOGDDVqYPfMtaIOixV2yP0BRAqyt35RM=`} alt="cover" className="w-24 h-24 rounded-full mx-auto" />
                         </div>
                     </div>
-                    <div className="modal-action">
+                    <div className="modal-action relative">
                         <label htmlFor="delete-appointment-modal" onClick={() => handleDelete(_id)} className="btn">Delete</label>
                     </div>
                 </div>
--- src/Components/Dashboard/AdminDashboard/ManageOrders/Modals/ConfirmOrderModal.jsx
+++ src/Components/Dashboard/AdminDashboard/ManageOrders/Modals/ConfirmOrderModal.jsx
@@ -26,6 +26,9 @@ const ConfirmOrderModal = ({ confirmOrder, setNumber, number }) => {
             <input type="checkbox" id="confirm-order-modal" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box max-w-5xl relative ">
+                    <div className="absolute top-0 h-full w-full left-0 mix-blend-darken opacity-5">
+                        <img src="https://res-console.cloudinary.com/dsigyjfjq/thumbnails/v1/image/upload/v1690616136/TG92ZXBpa19jb20tODMyMzU2ODk1LVRlY2hub2xvZ3lfc2hhZGluZ19ibHVlLXZpb2xldF9ncmFkaWVudF9saW5lX2VsZW1lbnRfZGVzaWduX2I3cWx5cQ==/preview" alt="" className="h-full w-full mix-blend-darken" />
+                    </div>
                     {
                         delivery_status !== "confirmed" &&
                         <div className="hidden md:block absolute bottom-3 left-3">
@@ -34,13 +37,13 @@ const ConfirmOrderModal = ({ confirmOrder, setNumber, number }) => {
                     }
                     <label htmlFor="confirm-order-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <div className="w-full flex gap-5 items-center">
-                        <h1 className='mb-4 badge badge-sm badge-warning text-sm font-bold text-white p-4'>Order History</h1>
-                        <h1 className='mb-4 text-3xl font-bold text-gray-700 p-4'>{products && products[0]?.name}</h1>
+                        <h1 className='mb-4 badge badge-sm badge-warning text-sm font-bold relative text-white p-4'>Order History</h1>
+                        <h1 className='mb-4 text-3xl font-bold text-gray-700 p-4 relative'>{products && products[0]?.name}</h1>
                         {/* <div className="w-full md:w-2/5 justify-end -mt-5 hidden md:block">
                             <img src={products && products[0]?.image} alt="cover" className="w-20 h-20 rounded-full mx-auto" />
                         </div> */}
                     </div>
-                    <div className="w-full flex flex-col md:flex-row justify-between items-start gap-3">
+                    <div className="w-full relative flex flex-col md:flex-row justify-between items-start gap-3">
                         <div className="">
                             <div className="w-full md:w-4/5 order-2 md:order-1">
                                 <h3 className="text-2xl font-bold pb-3">Product Info-</h3>
@@ -81,7 +84,7 @@ const ConfirmOrderModal = ({ confirmOrder, setNumber, number }) => {
                     </div>
                     {
                         delivery_status !== "confirmed" &&
-                        <div className="modal-action">
+                        <div className="modal-action relative">
                             <label htmlFor="confirm-order-modal" onClick={() => handleConfirmOrder(_id)} className="btn">Confirm</label>
                         </div>
                     }
