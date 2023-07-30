80d18692331839ec9b8db9b70f08cb38a7db45f5
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-07-30 13:04:11 +0600

    resolved gallery

--- src/Components/ProductDetails/OurGallery.jsx
+++ src/Components/ProductDetails/OurGallery.jsx
@@ -38,11 +38,11 @@ const OurGallery = () => {
     return (
         <div className='w-full'>
 
-            <div className="bg-gradient-to-r from-[#4C0144] to-[#0E000D] w-full">
+            <div className="bg-gradient-to-r from-[#0f667b] to-[#01404d] w-full">
                 <div className="flex flex-col md:flex-row gap-5 relative justify-around items-center py-20 px-5 md:px-20">
-                    <div className="absolute top-0 left-0 w-40 h-52 bg-[#6E2967]"></div>
+                    <div className="absolute top-0 left-0 w-40 h-52 bg-[#339bb5]"></div>
                     <div className="w-full md:w-3/5 relative">
-                        <div className="w-5/6 mx-auto md:m-0 bg-gradient-to-r from-[#42003B] p-7 rounded-2xl relative">
+                        <div className="w-5/6 mx-auto md:m-0 bg-gradient-to-r from-[#0f667b] p-7 rounded-2xl relative">
                             <img src="https://i.ibb.co/Fz1K8xd/Group-1.jpg" alt="" className="w-full mx-auto h-96" />
                         </div>
                     </div>
@@ -59,7 +59,9 @@ const OurGallery = () => {
                             }
                         </div>
                         <div onClick={() => setToggler(!toggler)} className="w-full flex justify-end absolute right-0 bottom-10 pt-10">
-                            <button className="flex w-32 py-2 group items-center justify-start pl-5 text-white bg-gray-600 rounded-l-3xl">View All <FaAngleRight className='text-2xl text-white group-hover:translate-x-3 duration-200 ease-in-out' /></button>
+                        <div className="bg-white pl-1 py-1 cursor-pointer rounded-l-3xl">
+                        <button className="flex w-32 py-2 group items-center justify-start pl-5 text-white bg-gray-600 rounded-l-3xl">View All <FaAngleRight className='text-2xl text-white group-hover:translate-x-3 duration-200 ease-in-out' /></button>
+                        </div>
                         </div>
                     </div>
                 </div>
