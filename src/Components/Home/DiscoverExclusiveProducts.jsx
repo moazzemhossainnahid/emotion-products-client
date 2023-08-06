989f20eb0002ca92f2e371c586e8b6542aae13ef
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-08-06 20:13:33 +0600

    translate eorking process

--- src/Components/Home/DiscoverExclusiveProducts.jsx
+++ src/Components/Home/DiscoverExclusiveProducts.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { FaArrowRight } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 const DiscoverExclusiveProducts = () => {
     const navigate = useNavigate();
+    const {t} = useTranslation();
+
+
     return (
         <div className=' w-full py-7'>
             <div className="w-full mx-auto bg-[url('https://i.ibb.co/Yb1FC84/Rectangle-174.png')] p-10 relative bg-cover">
                 <div className=" bg-gradient-to-b from-[#fcfcfc69] to-[#e5e4e4c8] p-7 rounded w-full md:w-4/5 mx-auto">
-                    <h3 className="text-gray-700 text-center font-bold text-2xl pb-7 md:text-3xl">Shine bright with our stunning lighting collection that illuminates your life</h3>
+                    <h3 className="text-gray-700 text-center font-bold text-2xl pb-7 md:text-3xl">{t('homeDiscoverExclusiveProducts.title')}</h3>
                     <div className="space-y-3">
-                        <p className='text-sm'>Nothing better than lighting to create the ambience you desire. With a perfect lighting fixture, you can complement your space. We offer a wide range of chandeliers, lanterns and other lighting fixtures that express your style and are as unique as you are.</p>
-                        <p className='text-sm'>Whether you are looking for a bohemian or an industrial style, our lighting fixtures are one of a kind. Further, our chandeliers will fit your style so that you can add drama to your dining area or a serene atmosphere to your bedroom.</p>
-                        <p className='text-sm'>Our lanterns are equally special, with various shapes and sizes. We promise to deliver the highest quality material so you don't regret buying any item. We design every lighting fixture with sturdy material so it lasts for years to come.</p>
-                        <p onClick={() => navigate("/lights")} className="text-sm font-bold cursor-pointer pt-5">Shop now and let us help you transform your home with our beautiful lighting.</p>
+                        <p className='text-sm'>{t('homeDiscoverExclusiveProducts.desc1')}</p>
+                        <p className='text-sm'>{t('homeDiscoverExclusiveProducts.desc2')}</p>
+                        <p className='text-sm'>{t('homeDiscoverExclusiveProducts.desc3')}</p>
+                        <p onClick={() => navigate("/lights")} className="text-sm font-bold cursor-pointer pt-5">{t('homeDiscoverExclusiveProducts.desc4')}</p>
                     </div>
                 </div>
             </div>
--- src/Components/Home/OurWorkingProcess.jsx
+++ src/Components/Home/OurWorkingProcess.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const OurWorkingProcess = () => {
+    const {t} = useTranslation();
+
 
     return (
         <div className='w-full text-center py-10 p-5'>
-            <h3 style={{ fontFamily: 'Silk Serif' }} className="text-2xl md:text-4xl pb-5 text-gray-700 ">We help you design your dream space in 3 simple steps </h3>
+            <h3 style={{ fontFamily: 'Silk Serif' }} className="text-2xl md:text-4xl pb-5 text-gray-700 ">{t('homeOurWorkingProcess.title')} </h3>
             {/* <p className="">It is a long established fact that a reader will be distracted by the readable content of a page</p> */}
             <div className="py-7 w-full md:w-4/5 mx-auto text-left flex flex-col md:flex-row gap-5 justify-between items-center">
                 <div className="space-y-3">
                     <div className="">
                         <img src="https://i.ibb.co/xFKXQtf/Group-200.png" alt="icon" />
                     </div>
-                    <h3 className="font-semibold text-xl">Consultation</h3>
-                    <p className="text-sm">The success of every project lies behind an efficient consultation. We walk through with you at every phase of the project from start to end. Our expert team understands all your queries and works with you to develop a plan that helps you achieve your goals. </p>
+                    <h3 className="font-semibold text-xl">{t('homeOurWorkingProcess.title1')} </h3>
+                    <p className="text-sm">{t('homeOurWorkingProcess.desc1')} </p>
                 </div>
                 <div className="space-y-3">
                     <div className="">
                         <img src="https://i.ibb.co/L0rgQ6z/Group-199.png" alt="icon" />
                     </div>
-                    <h3 className="font-semibold text-xl">Choose Design</h3>
-                    <p className="text-sm">No matter what your taste, we offer an extensive collection of designs to choose from. In our catalog, you can discover a design that will fit your style. Further, if you don't find a perfect design, you can get a personalized design that caters to your needs.</p>
+                    <h3 className="font-semibold text-xl">{t('homeOurWorkingProcess.title2')} </h3>
+                    <p className="text-sm">{t('homeOurWorkingProcess.desc2')} </p>
                 </div>
                 <div className="space-y-3">
                     <div className="">
                         <img src="https://i.ibb.co/9bq6z32/Group-201.png" alt="icon" />
                     </div>
-                    <h3 className="font-semibold text-xl">Work done</h3>
-                    <p className="text-sm">At Motion Products, we strive to meet our client's expectations. We pride ourselves on our dedicated team, which works to deliver projects on time and within your budget. So, stay calm and let us do all the work for you until your satisfaction.</p>
+                    <h3 className="font-semibold text-xl">{t('homeOurWorkingProcess.title3')} </h3>
+                    <p className="text-sm">{t('homeOurWorkingProcess.desc3')} </p>
                 </div>
             </div>
 
