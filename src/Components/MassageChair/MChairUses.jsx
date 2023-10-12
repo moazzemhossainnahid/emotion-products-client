d60368be95bc482c0f91e0ed39a4c5e2bd077aac
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-10-12 20:06:11 +0600

    resolved

--- src/Components/MassageChair/MChairUses.jsx
+++ src/Components/MassageChair/MChairUses.jsx
@@ -14,32 +14,36 @@ const MChairUses = () => {
                     <h3 className="text-2xl font-bold text-dark md:w-2/3">{t('mchair.uses.title')}</h3>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-7 py-7">
                         <div className="space-y-2">
-                            <div className="w-12 h-12 bg-[#22363C] p-3 rounded-full">
-                                <img src="https://i.ibb.co/ss8GzgH/Vector.png" alt="" className="w-full h-full flex justify-center items-center" />
+                            <div className="w-12 h-12 bg-[#22363C] p-2 rounded-full">
+                                <img src="https://i.ibb.co/H2zMCLt/1518185.png" alt="" className="w-full h-full flex justify-center items-center" />
                             </div>
-                            <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t1')}</h3>
+                            <h3 className="text-xl text-dark font-bold">Office</h3>
+                            {/* <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t1')}</h3> */}
                             <p className="text-gray-500 text-sm md:pr-10">{t('mchair.uses.d1')}</p>
                         </div>
                         <div className="space-y-2">
-                            <div className="w-12 h-12 bg-[#22363C] p-3 rounded-full">
-                                <img src="https://i.ibb.co/kKSXVCt/Vector-1.png " alt="" className="w-full h-full flex justify-center items-center" />
+                            <div className="w-12 h-12 bg-[#22363C] p-1 rounded-full">
+                                <img src="https://i.ibb.co/2ZqfbC8/25694.png" alt="" className="w-full h-full flex justify-center items-center" />
                             </div>
-                            <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t2')}</h3>
+                            <h3 className="text-xl text-dark font-bold">Home</h3>
+                            {/* <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t2')}</h3> */}
                             <p className="text-gray-500 text-sm md:pr-10">{t('mchair.uses.d2')}</p>
 
                         </div>
                         <div className="space-y-2">
-                            <div className="w-12 h-12 bg-[#22363C] p-3 rounded-full">
-                                <img src="https://i.ibb.co/xznr85F/69840.png" alt="" className="w-full h-full flex justify-center items-center" />
+                            <div className="w-12 h-12 bg-[#22363C] p-1 rounded-full">
+                                <img src="https://i.ibb.co/f8LgLZ3/69840.png" alt="" className="w-full h-full flex justify-center items-center" />
                             </div>
-                            <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t3')}</h3>
+                            <h3 className="text-xl text-dark font-bold">Gym</h3>
+                            {/* <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t3')}</h3> */}
                             <p className="text-gray-500 text-sm md:pr-10">{t('mchair.uses.d3')}</p>
                         </div>
                         <div className="space-y-2">
-                            <div className="w-12 h-12 bg-[#22363C] p-3 rounded-full">
-                                <img src="https://i.ibb.co/BVY5Q54/4327360.png" alt="" className="w-full h-full flex justify-center items-center" />
+                            <div className="w-12 h-12 bg-[#22363C] p-1 rounded-full">
+                                <img src="https://i.ibb.co/3yRD2k5/4327360.png" alt="" className="w-full h-full flex justify-center items-center" />
                             </div>
-                            <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t4')}</h3>
+                            <h3 className="text-xl text-dark font-bold">Airport</h3>
+                            {/* <h3 className="text-xl text-dark font-bold">{t('mchair.uses.t4')}</h3> */}
                             <p className="text-gray-500 text-sm md:pr-10">{t('mchair.uses.d4')}</p>
                         </div>
                     </div>
