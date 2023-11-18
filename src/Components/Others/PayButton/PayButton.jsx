cecbb9a84c151feddc5d1e70faf659a2b8fe42c0
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-11-18 11:48:10 +0600

    resolved

--- src/Components/Others/PayButton/PayButton.jsx
+++ src/Components/Others/PayButton/PayButton.jsx
@@ -6,24 +6,45 @@ import auth from '../../../../firebase.init';
 const PayButton = ({ checkoutItems }) => {
     const [user] = useAuthState(auth);
     console.log(checkoutItems);
-    const handleCheckout = () => {
 
-        axios.post(`https://emotion-products-server-iii.vercel.app/api/v1/stripe/create-checkout-session`, {
+    // const handleCheckout = () => {
+
+    //     axios.post(`https://emotion-products-server-iii.vercel.app/api/v1/stripe/create-checkout-session`, {
+    //         checkoutItems,
+    //         user,
+    //     }, {
+    //         method: 'POST',
+    //         mode:"no-cors",
+    //         headers: {
+    //             'content-type': 'application/json',
+    //             authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    //         }
+    //     }).then(res => {
+    //         if (res.data.url) {
+    //             window.location.href = res.data.url
+    //         }
+    //     }).catch((err) => console.log(err.message))
+    // }
+
+    const handleCheckout = () => {
+        // send to database
+        fetch(`https://emotion-products-server-iii.vercel.app/api/v1/stripe/create-checkout-session`, {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json",
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+
+            },
             checkoutItems,
             user,
-        }, {
-            method: 'POST',
-            mode:"no-cors",
-            headers: {
-                'content-type': 'application/json',
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
         }).then(res => {
             if (res.data.url) {
                 window.location.href = res.data.url
             }
         }).catch((err) => console.log(err.message))
+
     }
+
     return (
         <>
             <button onClick={() => handleCheckout()} className="text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white">Check Out</button>
